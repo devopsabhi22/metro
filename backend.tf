ae2a3f356dc984d14de45d8c357476a97400d979
devopsabhi22 <116897447+devopsabhi22@users.noreply.github.com>
2022-11-02 09:22:26 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "testresourcegroup1"
-    storage_account_name = "testaksterraform12"
+    storage_account_name = "testasgardaksterraformthanos"
     container_name       = "testtfstatedevops"
     key                  = "testtfstatedevops.tfstate"
   }
